a4939dbec863aaa0858cd3114a971e6bd8957784
fffukken <tamana.kenta@hotmail.co.jp>
2022-12-26 13:51:15 +0900

    initial commits

---
+++ 5_openjyanken/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+
+const io = socketIo(server);
+
+const PORT = 3000;
+
+// ルーティングの設定。'/' にリクエストがあった場合 src/index.html を返す
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// 3000番ポートでHTTPサーバーを起動
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('connectedだお');
+
+    socket.on('sendMessage', (message) => {
+        console.log('message has been sent:: ', message)
+        handsCheck(message)
+        io.emit('receiveMessage', message)
+    })
+});
+
+const handsCheck = (message) => {
+    console.log("start hand check")
+    console.log(message)
+    let player1SelectedValue = message["p1"]
+    console.log(player1SelectedValue)
+}
