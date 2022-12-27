0eb0c82a25cf78dbf8f469a047b82ebfbb8dc3d5
fffukken <tamana.kenta@hotmail.co.jp>
2022-12-27 13:58:31 +0900

    TODO: playerの手を永続化させる

--- 5_openjyanken/index.js
+++ 5_openjyanken/index.js
@@ -25,14 +25,37 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', (message) => {
         console.log('message has been sent:: ', message)
-        handsCheck(message)
-        io.emit('receiveMessage', message)
+        // handsCheck(message)
+        if (message["playerId"] === 0) {
+            var player0hand = message["playerHand"]
+        } else {
+            var player1hand = message["playerHand"]
+        }
+        console.log(player0hand, player1hand)
+        if (player0hand !== undefined && player1hand !== undefined) {
+            var battleResult = battle(player0hand, player1hand)
+            console.log(battleResult)
+        }
+        io.emit('receiveMessage', battleResult)
     })
 });
 
-const handsCheck = (message) => {
-    console.log("start hand check")
-    console.log(message)
-    let player1SelectedValue = message["p1"]
-    console.log(player1SelectedValue)
+function battle(player0hand, player1hand) {
+    if (player0hand === player1hand) {
+        return "hikiwake"
+    } else {
+        return "dottika kati"
+    }
 }
+
+// function handsCheck(message){
+//     console.log("start hand check")
+//     console.log(message)
+//     if (message["playerId"] === 0) {
+//         var playerhand = message["playerHand"]
+//     } else {
+//         var playerhand = message["playerHand"]
+//     }
+//     return playerhand
+//     // console.log(message, player0hand, player1hand)
+// }
