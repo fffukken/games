6264d81e14609ab66dce8994325473e73d4e20ac
fffukken <tamana.kenta@hotmail.co.jp>
2022-12-28 15:39:06 +0900

    copy from 5 open jyanken

---
+++ 6_speedjyanken/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const http = require('http');
+
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+
+const io = socketIo(server);
+
+const PORT = 3000;
+
+// ルーティングの設定。'/' にリクエストがあった場合 src/index.html を返す
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// 3000番ポートでHTTPサーバーを起動
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('connectedだお');
+
+    socket.on('sendMessage', (message) => {
+        console.log('message has been sent:: ', message)
+        // handsCheck(message)
+        for (const player of message) {
+            if (player["playerId"] === 0) {
+                var player0hand = player["playerHand"]
+            } else {
+                var player1hand = player["playerHand"]
+            }
+        }
+        console.log(player0hand, player1hand)
+        if (player0hand !== undefined && player1hand !== undefined) {
+            var battleResult = battle(player0hand, player1hand)
+            console.log(battleResult)
+            io.emit('receiveMessage', battleResult)
+        }
+    })
+});
+
+function battle(player0hand, player1hand) {
+    if (player0hand === player1hand) {
+        return "hikiwake"
+    } else if (player0hand === "ぱー" && player1hand === "ぐー") {
+        return "player0 win"
+    } else if (player0hand === "ぐー" && player1hand === "ちょき") {
+        return "player0 win"
+    } else if (player0hand === "ちょき" && player1hand === "ぱー") {
+        return "player0 win"
+    } else {
+        return "player1 win"
+    }
+}
+
+// function handsCheck(message){
+//     console.log("start hand check")
+//     console.log(message)
+//     if (message["playerId"] === 0) {
+//         var playerhand = message["playerHand"]
+//     } else {
+//         var playerhand = message["playerHand"]
+//     }
+//     return playerhand
+//     // console.log(message, player0hand, player1hand)
+// }
