cb9c6fa3c575151c691ce951c700eb08aa40590b
fffukken <tamana.kenta@hotmail.co.jp>
2023-01-06 14:39:26 +0900

    copy from 6 jyanken

---
+++ 7_indian/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+
+const socketIo = require('socket.io');
+
+const app = express();
+// 使用する画像ファイルをexpressサーバに追加
+// app.use('/img', express.static('img'));
+
+const server = http.Server(app);
+const io = socketIo(server);
+
+const PORT = 3000;
+
+// ルーティングの設定。'/' にリクエストがあった場合 src/index.html を返す
+app.get('/', (req, res) => {
+    console.log(__dirname)
+    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/img/janken_gu.png')
+});
+
+// 3000番ポートでHTTPサーバーを起動
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('connectedだお');
+
+    socket.on('sendMessage', (message) => {
+        console.log('message has been sent:: ', message)
+    })
+});
+
