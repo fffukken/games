d33c131847cb92e2dd056b5449c55f40dca0e19a
fffukken <tamana.kenta@hotmail.co.jp>
2023-01-17 09:02:09 +0900

    皇帝と奴隷のカードを作成する

--- 8_ecard/index.js
+++ 8_ecard/index.js
@@ -25,38 +25,43 @@ server.listen(PORT, () => {
 let playerHands = [];
 let message = "";
 
-const createPlayerHands = (token) => {
-    const randomHand = Math.trunc(Math.random() * 13) + 1;
-    playerHands.push({ "token": token, "hand": randomHand })
-    if (playerHands.length > 5) {
-        clearHands()
-        message += "ブラウザをリロードしてください"
+// 0:平民、1:皇帝,2:奴隷
+let emperorHands = [0, 0, 0, 0, 1];
+let slaveHands = [0, 0, 0, 0, 2]
+
+const shuffle = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 }
 
-const clearHands = () => {
-    playerHands = [];
-    //TODO： プレイヤー数をいい感じに調整したい
-
-    io.emit("reload", "");
-
+const createPlayerHands = () => {
+    playerHands = [{ "emperorHand": shuffle(emperorHands), "slaveHand": shuffle(slaveHands) }]
 }
+
+// const clearHands = () => {
+//     playerHands = [];
+//     //TODO： プレイヤー数をいい感じに調整したい
+//     io.emit("reload", "");
+// }
 
 
 io.on('connection', (socket) => {
     console.log('connected');
     // 特定のプレイヤーにだけ、自分のIDを渡す
-    io.to(socket.id).emit("token", { token: socket.id });
+    // io.to(socket.id).emit("token", { token: socket.id });
 
     // 全員に手札を渡し、HTML側で自分以外の手を非表示にする
     createPlayerHands(socket.id);
     io.emit("hands", playerHands);
 
     // 手札公開の際に改めて全プレイヤーの手札を送る。けど、この通信はなくせる気がする。
-    socket.on('requestShowHands', () => {
-        console.log('showhand ');
-        io.emit("showHands", playerHands);
-    });
+    // socket.on('requestShowHands', () => {
+    //     console.log('showhand ');
+    //     io.emit("showHands", playerHands);
+    // });
 
 });
 
