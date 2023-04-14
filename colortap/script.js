3c9df200221902590ccd08273e275c906c7d5234
fffukken <tamana.kenta@hotmail.co.jp>
2023-04-14 11:52:15 +0900

    add stages

--- colortap/script.js
+++ colortap/script.js
@@ -1,17 +1,24 @@
 const gameContainer = document.getElementById('game-container');
 const targetColorDisplay = document.getElementById('target-color');
-const colorTiles = document.querySelectorAll('.color-tile');
+let colorTiles = document.querySelectorAll('.color-tile');
 const scoreDisplay = document.getElementById('score-display');
 const timerDisplay = document.getElementById('timer-display');
 
 let score = 0;
-let timeRemaining = 30;
+let timeRemaining = 15;
 let timer;
 let targetColor;
 
-// Update the generateTargetColor function
+// Add the new color to the colors array
+const baseColors = ['red', 'blue', 'green', 'orange', 'purple', 'black', 'yellow', 'brown'];
+
+// Add a variable to track the current stage
+let currentStage = 1;
+
+// Add a variable to track the colors for the current stage
+let colors;
+
 function generateTargetColor() {
-    const colors = ['red', 'blue', 'green', 'orange'];
     targetColor = colors[Math.floor(Math.random() * colors.length)];
 
     // Set the text content
@@ -41,29 +48,90 @@ function updateScore() {
 }
 
 function startTimer() {
+    timerDisplay.textContent = `Time: ${timeRemaining}s`;
     timer = setInterval(() => {
         timeRemaining -= 1;
         timerDisplay.textContent = `Time: ${timeRemaining}s`;
+
         if (timeRemaining <= 0) {
-            clearInterval(timer);
             gameOver();
         }
     }, 1000);
 }
 
 function gameOver() {
+    clearInterval(timer);
+
+    if (score > 9) {
+        alert(`Congratulations! You have advanced to Stage ${currentStage + 1}`);
+        nextStage();
+    } else {
+        alert(`Game over! Your score: ${score}`);
+        initializeGame();
+    }
+}
+
+function nextStage() {
+    currentStage += 1;
+    colors = baseColors.slice(0, currentStage + 1);
+
+    // Reset the game state for the new stage
+    score = 0;
+    scoreDisplay.textContent = `Score: ${score}`;
+    timeRemaining = 15;
+
+    // Remove previous color tiles
+    colorTiles.forEach(tile => {
+        gameContainer.removeChild(tile);
+    });
+
+    // Add new color tiles
+    colors.forEach(color => {
+        const newColorTile = document.createElement('div');
+        newColorTile.className = 'color-tile';
+        newColorTile.id = color;
+        gameContainer.appendChild(newColorTile);
+        newColorTile.addEventListener('click', tileTapHandler);
+    });
+
+    // Update the colorTiles NodeList
+    colorTiles = document.querySelectorAll('.color-tile');
+
+    // Generate a new target color
+    generateTargetColor();
+
+    // Update timer display and clear the existing timer
+    timerDisplay.textContent = `Time: ${timeRemaining}s`;
+    // console.log(timerDisplay.textContent)
+    clearInterval(timer);
+
+    // Start a new timer
+    startTimer();
+}
+
+
+function initializeGame() {
+    // Set the colors for the current stage
+    colors = baseColors.slice(0, currentStage + 1);
+
+    // Update the color tiles event listeners
     colorTiles.forEach(tile => {
         tile.removeEventListener('click', tileTapHandler);
     });
-    alert(`Game over! Your score: ${score}`);
+
+    // Add new color tiles
+    for (let i = 0; i < colors.length; i++) {
+        colorTiles[i].id = colors[i];
+        colorTiles[i].addEventListener('click', tileTapHandler);
+    }
+
+    // Reset the game state for the new stage
+    score = 0;
+    scoreDisplay.textContent = `Score: ${score}`;
+    timeRemaining = 15;
+    startTimer();
+    generateTargetColor();
 }
 
-function initializeGame() {
-    colorTiles.forEach(tile => {
-        tile.addEventListener('click', tileTapHandler);
-    });
-    generateTargetColor();
-    startTimer();
-}
 
 initializeGame();
